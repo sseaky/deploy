02d5d2f74e80cc91cac1adb37984cd6923201b9e
seaky <seaky.cn@gmail.com>
2020-06-12 17:57:28 +0800

    bashit

---
+++ init/bashit.sh
@@ -0,0 +1,9 @@
+# @Author: Seaky
+# @Date:   2020-06-12 17:53:48
+# @Last Modified by:   Seaky
+# @Last Modified time: 2020-06-12 17:56:53
+
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh 
+
+sed -ir 's/bobby/pure/' ~/.bashrc
